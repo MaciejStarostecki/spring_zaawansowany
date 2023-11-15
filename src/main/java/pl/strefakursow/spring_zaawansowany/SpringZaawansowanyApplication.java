ecc30717782f04a91c6d5eefbf8c3e6b30ac96c9
Maciej Starostecki <maciejstarostecki@gmail.com>
2023-11-15 12:58:31 +0100

    * Dodanie testowego Task Schedulera

--- src/main/java/pl/strefakursow/spring_zaawansowany/SpringZaawansowanyApplication.java
+++ src/main/java/pl/strefakursow/spring_zaawansowany/SpringZaawansowanyApplication.java
@@ -2,8 +2,10 @@ package pl.strefakursow.spring_zaawansowany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringZaawansowanyApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/pl/strefakursow/spring_zaawansowany/component/CustomTaskSheduler.java
@@ -0,0 +1,21 @@
+package pl.strefakursow.spring_zaawansowany.component;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
+public class CustomTaskSheduler {
+
+//    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedDelay = 5000)
+//    @Scheduled(cron = "* * * * * *") //sekunda, minuta, godzina, dzień miesiąca, miesiąc, dzień tygodnia
+    @Scheduled(cron = "${cron.expression}")
+    public void doSomeStuff() {
+//        Thread.sleep(1000);
+        System.out.println("Some operation, time: " + LocalTime.now());
+    }
+
+}
